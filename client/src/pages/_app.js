f17d4c3cf425e3ee4950e8945238a4b82f1cdfd9
unknown <michaelpmaurerii@gmail.com>
2024-07-01 02:51:32 -0400

    got rid of useless files

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function App({ Component, pageProps }) {
   return (
     <UserProvider>
+      <Head>
+        <title>Shunli Chinese</title>
+      </Head>
       <Component {...pageProps}/>
     </UserProvider>
   );
